27dbec1ba1bd015e61039442a79fca76fdc3d009
Sergii Siryi <sergii.siryi97@gmail.com>
2023-09-09 08:31:49 +0200

    Add phpstan to project

---
+++ phpstan-baseline.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+$ignoreErrors = [];
+$ignoreErrors[] = [
+	'message' => '#^Unsafe usage of new static\\(\\)\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/src/SharedKernel/Id.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to an undefined method Symfony\\\\Component\\\\Validator\\\\ConstraintViolationListInterface\\:\\:getIterator\\(\\)\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/UI/API/Middleware/ErrorHandlerMiddleware.php',
+];
+
+return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
